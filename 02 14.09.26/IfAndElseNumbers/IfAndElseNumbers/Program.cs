9c35bbcebb6efa70a6494a75934e2cc24e94531a
ILMW69 <178587023+ILMW69@users.noreply.github.com>
2026-09-14 11:00:52 +0300

    IfAndElseNumbers

---
+++ 02 14.09.26/IfAndElseNumbers/IfAndElseNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+
+namespace IfAndElseNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjuta enda vanus");
+
+
+            /* peate kasutam aif and else,
+             * et kontrollida kas kasutaja on suurem
+             * või väiksem kui 18 */
+
+            string age_str = Console.ReadLine();
+            byte age = byte.Parse(age_str);
+
+            if (age >= 18)
+            {
+                Console.WriteLine("unc");
+            }
+            else 
+            {
+                Console.WriteLine("beebi");
+            }
+        }
+    }
+}
