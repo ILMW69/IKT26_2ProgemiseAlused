c44fb3aef25992f4413b3101b246e01c73021770
ILMW69 <178587023+ILMW69@users.noreply.github.com>
2026-09-14 09:58:08 +0300

    IfAndElse

---
+++ 02 14.09.26/IfAndElse/IfAndElse/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace IfAndElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta enda nimi");
+
+            /*siin on muutuja name, mis on string
+             * loab andmeid konsioolist ja salvestab
+             * need uutuja name sisse */
+            string name = Console.ReadLine();
+            
+            // kui muutuja on tühi, siis väljastab konsoolile errori
+            if (name != "")
+            {
+                Console.WriteLine("Tere, " + name);
+            }
+            else
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine("ERROR: Nimi sisestamata!");
+                for (byte i=0; i < 3; i++)
+                {
+                    Console.Beep();
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
