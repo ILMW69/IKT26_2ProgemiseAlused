abee408cd1104efddedc334ed0c7d4aa264fadf9
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-21 10:57:22 +0300

    IfAndElseColors

---
+++ 03 21.09.26/IfAndElseColours/IfAndElseColours/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace IfAndElseColours
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleColor[] all_colors = [ConsoleColor.Red, ConsoleColor.Green, ConsoleColor.Blue, ConsoleColor.White];
+
+            Console.BackgroundColor = all_colors[new Random().Next(all_colors.Length)];
+
+            Console.WriteLine("Värvide valik: Red, Green, Blue ja White");
+            Console.WriteLine("Mis värvi on tekst");
+
+            string värv = Console.ReadLine();
+
+
+            if (Console.BackgroundColor.ToString() == värv)
+            {
+                Console.WriteLine("Õige!");
+            }
+            else
+            {
+                if (Enum.TryParse<ConsoleColor>(värv, out _))
+                {
+                    Console.WriteLine("Vale!");
+                }
+                else
+                {
+                    Console.WriteLine("invalid");
+                }
+            }
+        }
+    }
+}
